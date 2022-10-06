25dc5b60a41f3dc0fd6a1048daddd549de8ea57f
Marcelo A Tabone <marceloatabone@hotmail.com>
2022-10-06 18:51:06 -0300

    Add database support

---
+++ src/main/kotlin/com/springh2/test/repository/MessageRepository.kt
@@ -0,0 +1,11 @@
+package com.springh2.test.repository
+
+import com.springh2.test.model.Message
+import org.springframework.data.jdbc.repository.query.Query
+import org.springframework.data.repository.CrudRepository
+
+interface MessageRepository : CrudRepository<Message, String>{
+
+    @Query("select * from messages")
+    fun findMessages(): List<Message>
+}
